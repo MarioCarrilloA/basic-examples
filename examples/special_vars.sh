118f48e7ab6434935827d0d2bab4773e402d6a0c
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2016-04-27 10:17:21 -0500

    Add more examples about special vars
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

--- examples/special_vars.sh
+++ examples/special_vars.sh
@@ -4,6 +4,7 @@
 # that has been received
 if (( $# > 0 ));
 then
+
 	# [$@] var contains all arguments
 	# written bv the user as parameters
 	echo "Arguments received: $#"
@@ -11,12 +12,33 @@ then
 	do
 		echo "Argument: $ARG"
 	done
+
 	# [$*] var contains all argumets
 	# The difference between $@ and $* is:
 	# [$@] is equivalent: "$1" "$2" "$3" ...
 	# [$*] is equivalent: "$1 $2 $3"
 	echo "The argument received were: $*"
 	echo "The first argument was: $1"
+
+	# [$!] var contains the PID (process
+	# identifier) of the most recently command
+	# executed in background in this bash session.
+	uname -r &
+	echo "PID of last bg command: $!"
+
+	# [$$] var contains the PID (process
+	# identifier) of the most recently command
+	# executed in this bash session.
+	uname -a
+	echo "PID of last command: $$"
+
+	# [$?] var contains the exit status of the most
+	# recently executed foreground pipeline.
+	uname -s
+	echo "Exit code $?"
+
 else
 	echo "No arguments"
 fi
+
+exit 0
