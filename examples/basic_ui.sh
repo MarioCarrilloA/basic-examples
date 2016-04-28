0572af92656559a2cc06bad0368ba99be5da202a
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2016-04-28 09:58:07 -0500

    Add example using dialog
    dialog command allows to create basic
    user interfaces in command line using
    ncurses library.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

---
+++ examples/basic_ui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# In bash is posible to create basic
+# user interfaces, it could help to create
+# a user-friendly program, this application
+# called dialog uses ncurses library for that.
+
+INFO="info.txt"
+
+dialog  --title "Data Registration" \
+		--separate-widget $'\n' \
+		--inputbox "Name:" 8 40 \
+		--inputbox "Email:" 8 40 \
+		--inputbox "profile:" 8 40 \
+		--radiolist "Select Course:" 10 40 4 \
+        	1 Linux on \
+			2 Iot off \
+			3 other off 2>"$INFO"
+
+info=$(cat "$INFO")
+echo "$INFO"
+dialog --title "Personal information" --msgbox "$info" 10 25 ;
