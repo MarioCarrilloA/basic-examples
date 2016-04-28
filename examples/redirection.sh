bd48a15901ac56a50ef9cd273d173ad4dde5710f
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2016-04-28 02:01:33 -0500

    redirection to /dev/null

--- examples/redirection.sh
+++ examples/redirection.sh
@@ -47,3 +47,5 @@ echo "This is the first line"  > myoutput2.txt
 echo "This is the first line again"  > myoutput2.txt
 echo "This is the first line again again"  > myoutput2.txt
 
+# Redirect errors to /dev/null
+ls superman 2> /dev/null
