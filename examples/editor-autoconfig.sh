4b0f43f33d1e87e6c5c5f149081dbe1b6e775469
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2016-04-27 23:38:19 -0500

    add editor autoconfig
    The second and third line is used by emacs and vi repectibely to configure itself

---
+++ examples/editor-autoconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh nu
+#
+
+echo "Homework:"
+echo
+echo "What is the meaning from the second and third line of:"
+head -n 3 $0
+
+
+
