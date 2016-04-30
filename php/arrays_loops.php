14a4c191fc1448484fa32622e2869d6d56c8d5b3
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-04-30 12:52:13 -0500

    php: Add example using an index var
    An alternative to "foreach" it is possible
    to use a var index in order to interate an
    array in php.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- php/arrays_loops.php
+++ php/arrays_loops.php
@@ -7,15 +7,23 @@
 * could be an integer number, a float number,
 * a boolean or even a string.
 */
+	echo "Operating Systems\n";
 	$osystems = [
-		1      => "Fedora",
-		2      => "Ubuntu",
-		"tres" => "Debian",
-		4.5    => "Clear Linux",
-		true   => "CentOS",
+		1       => "Fedora",
+		2       => "Ubuntu",
+		"tres"  => "Debian",
+		FALSE   => "CentOS",
+		4.5     => "Clear Linux",
 	];
 
 	foreach ($osystems as $i => $value) {
-	print ("Index: ".$i."\tValue: ".$value."\n");
-}
+		print ("Index: ".$i."\tValue: ".$value."\n");
+	}
+
+	echo "Tools\n";
+	$tools = [ "gcc", "g++", "cc" ];
+
+	for ($i = 0; $i < count($tools); $i++) {
+		print ("Index: ".$i."\tValue: ".$tools[$i]."\n");
+	}
 ?>
