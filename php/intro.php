8df2f4b5700ccb24fbacd3d1466589512471e17d
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-04-30 11:22:12 -0500

    Add an introduction php script
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

---
+++ php/intro.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+
+<?php
+
+/*
+* PHP (recursive acronym for PHP: Hypertext Preprocessor)
+* is a widely-used open source general-purpose scripting
+* language that is especially suited for web development
+*  and can be embedded into HTML.
+*/
+	$course = "Linux";
+
+	echo "Hello world!\n";
+	echo "This is my \"first\" program in php\n";
+	print ("Course name: ".$course."\n");
+?>
