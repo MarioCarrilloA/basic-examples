f1b0a87efb3a3fe56d4ad7e5a355c9a79f104d31
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-04-30 12:27:47 -0500

    bash: Add exit code to pipes script.
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- bash/pipes.sh
+++ bash/pipes.sh
@@ -4,14 +4,15 @@
 #
 # source: http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-4.html
 #
-# Pipes let you use the output of a program as the input of another one 
-# 
+# Pipes let you use the output of a program as the input of another one
+#
 
 # first the command ls -l is executed, and it's output, instead of being printed,
-# is sent (piped) to the sed program, which in turn, prints what it has to. 
+# is sent (piped) to the sed program, which in turn, prints what it has to.
 ls -l | sed -e "s/[aeio]/u/g"
 
 # Here, the output of the program ls -l is sent to the grep program, which,
-# in turn, will print lines which match the regex "\.txt$". 
+# in turn, will print lines which match the regex "\.txt$".
 ls -l | grep "\.txt$"
 
+exit 0
