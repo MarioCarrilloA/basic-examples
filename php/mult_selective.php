d5dc43cd7b6d58351cf807d62f2f68ad64d4104e
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-04-30 13:57:41 -0500

    php: Add example for selective multiple.
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

---
+++ php/mult_selective.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+
+<?php
+
+	$opt = 2;
+
+	switch ($opt) {
+    	case 0:
+        	echo "case 0\n";
+			break;
+		case 1:
+			echo "case 1\n";
+			break;
+		case 2:
+			echo "case 2\n";
+			break;
+		default:
+			echo "Invalid\n";
+			break;
+	}
+?>
