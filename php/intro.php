1e77cf5b91656b2f5a1070b5f73e84695f699481
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2016-05-23 11:03:48 -0500

    php: Add php link about official documentation
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

--- php/intro.php
+++ php/intro.php
@@ -6,7 +6,8 @@
 * PHP (recursive acronym for PHP: Hypertext Preprocessor)
 * is a widely-used open source general-purpose scripting
 * language that is especially suited for web development
-*  and can be embedded into HTML.
+* and can be embedded into HTML.
+* Offical documentation: http://php.net/download-docs.php
 */
 	$course = "Linux";
 
