387fe1c10e162c54ed95a8a9b8beb4b4118f2368
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-05-24 09:49:48 -0500

    mk: Add example using mk rules
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

---
+++ mk/mk_rules.mk
@@ -0,0 +1,14 @@
+CC=gcc
+CFLAGS=-I./src
+DEPS = ./src/calc.h
+OBJ = calc.o main.o 
+
+%.o: src/%.c $(DEPS)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+mytest: $(OBJ)
+	gcc -o $@ $^ $(CFLAGS)
+
+clean:
+	rm -f *.o mytest 
+
