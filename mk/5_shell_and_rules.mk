aa0e5a25ced024ead0b1934e2b696f960fbb1730
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-05-25 10:08:56 -0500

    mk: Add rule for running image
    this example shows an example about
    how some tasks can be automated
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- mk/5_shell_and_rules.mk
+++ mk/5_shell_and_rules.mk
@@ -1,25 +1,35 @@
-IMAGE ?= clear-8000-kvm.img.xz
+IMAGE ?= clear-8000-live.img.xz
 CLURL := https://download.clearlinux.org/releases/8000/clear/
 CLEAR_URL := $(CLURL)/$(IMAGE)
+OVMF_URL := https://download.clearlinux.org/image/OVMF.fd
 
 all: download decompress run
 
 decompress:
+	@echo "Decompressing image"
 	$(shell xz -d $(IMAGE))
 
 download:
 	@set -e
-	@if [ -f "$$IMAGE" ];then \
+	@if [ -f $(IMAGE) ];then \
 		echo "The image exists"; \
 	else \
 		echo "The image does not exist"; \
 		echo "The image will be downloaded"; \
 		curl -O $(CLEAR_URL); \
+		curl -O $(OVMF_URL); \
 	fi;	
 
 run:
 	@set -e
-	@uname -a
+	@qemu-system-x86_64 \
+		-enable-kvm \
+		-bios OVMF.fd \
+		-smp cpus=4,cores=2 -cpu host \
+		-vga none -nographic \
+		-drive file=clear-8000-live.img,if=virtio,aio=threads \
+		-net user -net nic,model=virtio -net user,hostfwd=tcp::10022-:22 \
+		-debugcon file:debug.log -global isa-debugcon.iobase=0x402
 
 showsystem:
 	@uname -a
