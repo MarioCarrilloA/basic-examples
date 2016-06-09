2d6f1df78a8c69a1bd3ebe41e5d2db42d5d8256d
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2016-06-09 17:11:47 -0500

    go: Add example using conditionals
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

---
+++ go/4_conditionals.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func number_type(number int) {
+
+	//fmt.Printf("The number %d\n",number)
+	if number < 0 {
+		fmt.Printf("The number %d is negative\n", number)
+	}else if number == 0 {
+		fmt.Printf("The number %d is zero\n", number)
+	}else {
+		fmt.Printf("The number %d is positive\n", number)
+	}
+}
+
+func main() {
+	number_type(0)
+	number_type(-1)
+	number_type(89)
+}
