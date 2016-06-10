2e3813267795091b3fd495b146a571c9dd9f1d69
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2016-06-10 12:46:57 -0500

    go: Add example using maps
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

---
+++ go/6_maps.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type str_example struct {
+	name string
+	age int
+}
+
+func maps_and_structs() {
+	var map_example = map[int]str_example{
+		1: {"Bill",  23},
+		2: {"Jeny",  27},
+		3: {"three", 32},
+	}
+
+	for key, data := range map_example {
+		fmt.Println("key:", key, "data:", data.name, data.age)
+	}
+	delete(map_example, 2)
+	fmt.Println(map_example)
+}
+
+func basic_maps() {
+	var map_example map[string]int
+	map_example = make(map[string] int)
+	map_example["one"]   = 1
+	map_example["two"]   = 2
+	map_example["three"] = 3
+	fmt.Println(map_example)
+	for key, data := range map_example {
+        fmt.Println("Key:", key, "data:", data)
+    }
+}
+
+func main() {
+	fmt.Println("Maps in go language")
+	basic_maps()
+	maps_and_structs()
+}
