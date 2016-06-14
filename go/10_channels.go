95ddbae3be17198f9b0a3375e1788a136987ca6a
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-06-14 09:53:34 -0500

    go: Add example using a buffer in channel
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- go/10_channels.go
+++ go/10_channels.go
@@ -6,10 +6,21 @@ func add_list(name string, list chan string) {
 	list <- name
 }
 
+func buffer_chan() {
+	nums := make(chan int, 2)
+	nums <- 1
+	nums <- 2
+//	nums <- 3
+	fmt.Println(<- nums)
+	fmt.Println(<- nums)
+//	fmt.Println(<- nums)
+}
+
 func main() {
 	list := make(chan string)
 	go add_list("Joe", list)
 	go add_list("Bill", list)
 	n1, n2 := <-list, <-list
 	fmt.Println(n1, n2)
+	buffer_chan()
 }
