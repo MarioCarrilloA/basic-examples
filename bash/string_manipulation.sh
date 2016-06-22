bba2d89059c087880cd3ce8a30da7fd27d4a2154
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-06-22 12:49:11 -0500

    bash: Add example using regex
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

---
+++ bash/string_manipulation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+EXAMPLE="abcABC123ABCabc123"
+SUBS="ABC"
+
+# Using regular expresions
+echo "${#EXAMPLE}"
+echo "${EXAMPLE:1}"
+echo "${EXAMPLE:7}"
+echo "${EXAMPLE:8:12}"
+echo "${EXAMPLE##$SUBS}"
