ad8c7c38679f785910c8fddfe769de7844a1de72
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-12-12 23:47:09 -0600

    perl: Add some doc about vars in perl
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- perl/2_vars.pl
+++ perl/2_vars.pl
@@ -1,8 +1,29 @@
 #!/usr/bin/perl
 
+# Some about vars in perl
+#
+# - The most basic kind of variable
+#   in Perl is the scalar variable.
+# - It is possible to interchange
+#   different kind of values in the
+#   same var.
+
+
+# Assign a string
 $figure = "Circle";
-$radio = 50;
+
+# Assign an integer
+$radio = 20;
+
+# Assign a float
 $PI = 3.1416;
 
+# The var type will be according
+# to the received value, even if
+# another type of data is involved
 $area = ($PI * ($radio**2));
-print "Area: $area\n";
+
+#interchange value
+$radio=$figure;
+
+print "Area of $radio is: $area\n";
