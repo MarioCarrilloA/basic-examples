87ee75fd603b05e93ce9129f9c0ea9467e8625c1
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2017-01-11 22:50:16 -0600

    perl: Add more examples about conditionals
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- perl/3_conditional.pl
+++ perl/3_conditional.pl
@@ -5,4 +5,22 @@ $B = 20;
 
 $res = ($B > $A )? "B is greater than A" : "A is greater than B";
 
-print "$res\n"
+print "$res\n";
+
+$year = 2017;
+
+if ($year gt 0) {
+	print "The year is $year\n";
+} else {
+	print "Invalid year\n";
+}
+
+if (($year % 100) > 0 || ($year % 400) > 0) {
+	print "No bisiest year\n";
+} else {
+	print "Bisiest year\n";
+}
+
+unless (!$#ARGV) {
+	print "No arguments\n"
+}
