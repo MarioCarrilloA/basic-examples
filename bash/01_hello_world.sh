094edc87d862dd538e26714b204b33c6e5599ea9
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2018-03-02 17:22:31 +0000

    bash: update help comments in script
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

--- bash/01_hello_world.sh
+++ bash/01_hello_world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# NOTE: The first line of this script is known as “shebang”.
+# The objective of this line is to invoke the appropriate
+# interpreter. The use of shebang is not exclusive of bash.
+
 echo "Hello world!"
