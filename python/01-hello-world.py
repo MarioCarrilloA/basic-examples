452d5a5bef43176ea8754cf9fd8276e9e35a17fb
Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>
2018-03-02 20:46:02 +0000

    python: getting started
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.alfredo.c.arevalo@intel.com>

---
+++ python/01-hello-world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# NOTES: The first line (shebang) specify the interprter that
+# will be invoked. The python files assume to use UTF-8 encoding
+# by default however they can be written and loaded using ASCII too.
+print("Hello", "World!")
---
+++ python/README.md
@@ -0,0 +1,4 @@
+# Basic examples - Python
+
+This section includes a set of basic python examples. The objective of these
+scripts is to give a very basic overview about the language.
